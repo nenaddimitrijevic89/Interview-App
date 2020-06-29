8120aac257ed03cb7685bbcb6eafab73db498104
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-06-29 22:56:17 +0200

    header is finished

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
 import { Homepage } from './components/Homepage/Homepage';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Homepage />
     </div>
   );
