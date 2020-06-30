371132886cc603bd81bd434409b686242a342c76
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-06-30 22:16:52 +0200

    search first step done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
 import { Homepage } from './components/Homepage/Homepage';
-import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <div>
-      <Header />
       <Homepage />
     </div>
   );
---
+++ src/components/Report/Reports.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Reports extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
