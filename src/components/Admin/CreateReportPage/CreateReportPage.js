28f4324412e4503f4750edbec1753c50d5403d02
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-07-08 18:48:47 +0200

    nav list created

--- src/components/Admin/CreateReportPage/CreateReportPage.js
+++ src/components/Admin/CreateReportPage/CreateReportPage.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { serviceCandidate } from '../../../services/fetchCandidates';
 import { Header } from '../../Header/Header';
-import { serviceReports } from '../../../services/fetchReports';
 import { serviceCompanies } from '../../../services/fetchCompanies';
 import { Container, Col, Row } from 'react-materialize';
 import { CandidatesSelector } from './CandidatesSelector/CandidatesSelector';
+import { Search } from '../../Search/Search';
+import style from './CreateReportPage.module.css';
+import { search } from '../../../shared/utilities';
+import { NavList } from './NavList/NavList';
 
 class CreateReportPage extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             candidates: [],
+            filteredCandidates: [],
             companies: []
         }
     }
+
     componentDidMount() {
         serviceCandidate.getCandidates()
-            .then(response => this.setState({ candidates: response }))
+            .then(response => this.setState({ candidates: response, filteredCandidates: response }))
 
         serviceCompanies.getCompanies()
             .then(response => this.setState({ companies: response }))
+    }
+
+    searchData = (text) => {
+        let filtered = search(this.state.candidates, ['name'], text)
+        this.setState({ filteredCandidates: filtered })
     }
 
     render() {
@@ -29,10 +39,11 @@ class CreateReportPage extends React.Component {
                 <Container>
                     <Row>
                         <Col l={3}>
-
+                            <NavList />
                         </Col>
-                        <Col l={9}>
-                            <CandidatesSelector candidates={this.state.candidates} />
+                        <Col l={9} className={style.selectorWrapper}>
+                            <Search search={this.searchData} />
+                            <CandidatesSelector candidates={this.state.filteredCandidates} />
                         </Col>
                     </Row>
                 </Container>
---
+++ src/components/Admin/CreateReportPage/NavList/NavList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Row } from 'react-materialize';
+
+const NavList = () => {
+    return (
+        <>
+            <Row><h3>Candidate</h3></Row>
+            <Row><h3>Company</h3></Row>
+            <Row><h3>Description</h3></Row>
+        </>
+    )
+}
+
+export { NavList };
